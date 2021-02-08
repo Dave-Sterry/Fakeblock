027c2dd3c4f817c5905187476f95118dd8bf08c0
David Sterry <sterry.david@gmail.com>
2021-02-08 11:55:17 -0800

    installed bootstrap
    Co-authored-by: Taylor Delph <taylulzcode@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Home from "./home";
+import Messages from "./messages";
+import Notifications from "./notifications";
 
 function Header(){
   return(
-    <h1>Fakeblock</h1>
-  );
+    <React.Fragment>
+        <h1>Fakeblock</h1>
+        <Home />
+        <Messages />
+        <Notifications />
+      </React.Fragment>
+    );
 }
 
 export default Header;
